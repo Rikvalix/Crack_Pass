310f0692ea1a7495e137dc142c9f2db9b3143173
Rikvalix <titouan.delion.desrochers@outlook.fr>
2023-12-16 11:49:11 +0100

    ajout du gitignore

--- Brute_forcepy.py
+++ Brute_forcepy.py
@@ -2,6 +2,6 @@ import hasheur
 
 #cd9f4ada5e2a97ec6def6555476524712760e3d8ee99c26ec2f11682a1194778
 
-sha = hasheur.AttaqueBruteForce("cd9f4ada5e2a97ec6def6555476524712760e3d8ee99c26ec2f11682a1194778","SHA-256")
+sha = hasheur.AttaqueBruteForce("8c9bb5636e65e041b17c3617c95c2ce83e8319f943341e70fa478a4b404fdea8","SHA-256")
 a = sha.Brute_Force('')
 print(a)
